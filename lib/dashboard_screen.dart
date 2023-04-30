ba26f8d232758f24b2b3bf53ffd0c6835c6d183a
Shakti184 <shaktimaddeshiya184@gmail.com>
2023-04-30 18:38:19 +0530

    fixes

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -371,6 +371,7 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Colors.white,
       shadowColor: Colors.blueAccent,
       child: SingleChildScrollView(
         child: Column(
@@ -385,7 +386,34 @@ class NavigationDrawer extends StatelessWidget {
   }
   
   buildHeader(BuildContext context) =>Container(
-    padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top,
+    color: Colors.blue.shade800,
+    padding: EdgeInsets.only(
+      top: 24+ MediaQuery.of(context).padding.top,
+      bottom: 24,
+    ),
+    child:Column(
+      children: const [
+        CircleAvatar(
+          radius: 52,
+          backgroundImage:ExactAssetImage('assets/img1.jpg'),
+        ),
+        SizedBox(height: 12,),
+        Text(
+          'Plant Doctor',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          'P_Doc123@gmail.com',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+          ),
+        ),
+
+      ],
     ),
   );
   buildMenuItem(BuildContext context)=>Container(
@@ -393,34 +421,34 @@ class NavigationDrawer extends StatelessWidget {
     child: Wrap(
       children: [
         ListTile(
-          leading: const Icon(Icons.home),
-          title: const Text('Home'),
+          leading: const Icon(Icons.home_max_rounded,color: Colors.black,),
+          title: const Text('Home',),
           onTap: (){},
         ),
         ListTile(
-          leading: const Icon(Icons.notifications),
-          title: const Text('Menu'),
+          leading: const Icon(Icons.notifications_active_outlined,color: Colors.black,),
+          title: const Text('Notifications'),
           onTap: (){},
         ),
         const Divider(color: Colors.black,),
         ListTile(
-          leading: const Icon(Icons.update),
+          leading: const Icon(Icons.update,color: Colors.black,),
           title: const Text('Update'),
           onTap: (){},
         ),
+        // ListTile(
+        //   leading: const Icon(Icons.favorite_border,color: Colors.black,),
+        //   title: const Text('Favourites'),
+        //   onTap: (){},
+        // ),
         ListTile(
-          leading: const Icon(Icons.favorite),
-          title: const Text('Favourites'),
+          leading: const Icon(Icons.settings,color: Colors.black,),
+          title: const Text('Settings'),
           onTap: (){},
         ),
         ListTile(
-          leading: const Icon(Icons.menu_open_outlined),
-          title: const Text('Menu'),
-          onTap: (){},
-        ),
-        ListTile(
-          leading: const Icon(Icons.menu_open_outlined),
-          title: const Text('Menu'),
+          leading: const Icon(Icons.info_outline,color: Colors.black,),
+          title: const Text('About'),
           onTap: (){},
         ),
       ],
